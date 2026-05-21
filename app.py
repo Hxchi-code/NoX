072c1c7875a44bbdfef6813febb202f743d71520
Hxchi-code <quinjengkol@gmail.com>
2026-05-22 01:19:00 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,31 +1,26 @@
 from flask import Flask, render_template, request, send_file
-import struct
-import io
+import struct, io
 
 app = Flask(__name__)
-
-def process_video_logic(data, mode):
-    data = bytearray(data)
-    if mode == 'bug':
-        stts_idx = data.find(b'stts')
-        if stts_idx != -1:
-            entry_offset = stts_idx + 12 + 4
-            if len(data) > entry_offset + 4:
-                data[entry_offset:entry_offset+4] = struct.pack('>I', 0xFFFF)
-    return data
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/convert', methods=['POST'])
-def convert():
+@app.route('/process', methods=['POST'])
+def process():
     file = request.files['video']
     mode = request.form.get('mode')
-    data = process_video_logic(file.read(), mode)
+    data = bytearray(file.read())
+
+    if mode == 'bug':
+        stts_idx = data.find(b'stts')
+        if stts_idx != -1:
+            # Inject 0xFFFF ke entry pertama durasi frame
+            data[stts_idx + 16:stts_idx + 20] = struct.pack('>I', 0xFFFF)
     
     output = io.BytesIO(data)
     return send_file(output, mimetype='video/mp4', as_attachment=True, download_name=f"{mode}_{file.filename}")
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
